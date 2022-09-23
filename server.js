2a21e05102ac948e9c2adfe362e84a03cba91d50
MistbornL <lashaloria3@gmail.com>
2022-09-23 17:00:24 +0400

    connected to server

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
+const mongoose = require("mongoose");
+const path = require("path");
 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+const app = express();
+
+app.use(express.json({ extended: true }));
+
+// app.use("/api/auth", require("./routes/auth.routes"));
+// app.use("/api/users", require("./routes/users.routes"));
+
+const PORT = process.env.PORT || 5000;
+const URI = process.env.ATLAS_URI;
+const start = async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server error", e.message);
+    process.exit(1);
+  }
+};
+
+start();
